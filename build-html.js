9fdd905c0c5f87733668392fc06e4bf19548b9ab
Matthew Wright <matthew@smallhelm.com>
2017-12-30 09:25:38 -0600

    fixing linux download links

--- build-html.js
+++ build-html.js
@@ -41,10 +41,10 @@ httpGetLatest(function(err, data){
     data.assets.forEach(function(a){
         var url = a.browser_download_url;
         var push = function(os, label){
-            info.btns_by_os[os].push(btnTemplate({
+            info.btns_by_os[os].push({
                 label: label,
                 url: url
-            }));
+            });
         };
 
         if(/\.exe$/i.test(url)){
@@ -53,15 +53,45 @@ httpGetLatest(function(err, data){
         if(/\.dmg$/i.test(url)){
             return push("mac", ".dmg");
         }
-        if(/\.AppImage$/i.test(url)){
-            return push("linux", ".AppImage");
+        if(/x86_64\.AppImage$/i.test(url)){
+            return push("linux", ".AppImage (64)");
         }
-        if(/\.deb$/i.test(url)){
-            return push("linux", ".deb");
+        if(/i386\.AppImage$/i.test(url)){
+            return push("linux", ".AppImage (32)");
         }
-        if(/\.tar\.xz$/i.test(url)){
-            return push("linux", ".tar.xz");
+        if(/amd64\.deb$/i.test(url)){
+            return push("linux", ".deb (64)");
         }
+        if(/i386\.deb$/i.test(url)){
+            return push("linux", ".deb (32)");
+        }
+        if(/ia32\.tar\.bz2$/i.test(url)){
+            return push("linux", ".tar.bz2 (32)");
+        }
+        if(/\.tar\.bz2$/i.test(url)){
+            return push("linux", ".tar.bz2 (64)");
+        }
+    });
+
+    var linuxDLSortScore = function(a){
+        var score = 0;
+        if(/tar/.test(a.label)){
+            score += 20;
+        }else if(/deb/.test(a.label)){
+            score += 10;
+        }
+        if(/64/.test(a.label)){
+            score += 1;
+        }
+        return score;
+    };
+
+    info.btns_by_os["linux"].sort(function(a, b){
+        return linuxDLSortScore(b) - linuxDLSortScore(a);
+    });
+
+    Object.keys(info.btns_by_os).forEach(function(os){
+        info.btns_by_os[os] = info.btns_by_os[os].map(btnTemplate);
     });
 
     var src = fs.readFileSync(path.resolve(__dirname, "./index.ejs"), "utf8");
